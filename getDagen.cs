cc6704b82d08b3d3a7d24d5dbcbdd045cafc425d
DemortierIbe <ibe.demortier@student.howest.be>
2022-09-29 11:56:04 +0200

    commit1

---
+++ getDagen.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace MCT.Functions
+{
+    public static class getDagen
+    {
+        [FunctionName("getDagen")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "days")] HttpRequest req,
+            ILogger log)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            List<string> dagen = new List<string>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "select Distinct DagVanDeWeek From Bezoekers";
+
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        var dag = reader["DagVanDeWeek"].ToString();
+                        dagen.Add(dag);
+                    }
+                }
+            }
+
+            return new OkObjectResult(dagen);
+        }
+    }
+}
